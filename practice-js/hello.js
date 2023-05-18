004c48af194c670ae6efbb9061150495d3af7a39
G184072021 <218407@st.takushoku-u.ac.jp>
2023-05-18 16:10:53 +0900

    a

--- practice-js/hello.js
+++ practice-js/hello.js
@@ -1,1 +1,8 @@
 console.log('こんにちは');
+let x;
+
+x=3;
+
+x=x+1;
+
+console.log(x);
--- practice-js/obj-practice.js
+++ practice-js/obj-practice.js
@@ -15,4 +15,10 @@ let gakka = [
 ];
 
 //////////////// ここから下にプログラムを書きたそう!
-
+console.log(campus.address);
+for(let s of campus.buildingD){
+	console.log(s);
+}
+for(let n of gakka){
+	console.log(n.name)
+}
