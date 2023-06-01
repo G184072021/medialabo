5bc72a3455c41a1c491d62aacaea03b15953c551
G184072021 <218407@st.takushoku-u.ac.jp>
2023-06-01 15:20:34 +0900

    a

--- kazuate.js
+++ kazuate.js
@@ -4,7 +4,7 @@ console.log('答え（デバッグ用）: ' + kotae);
 
 // 入力回数（予想回数）
 let kaisu = 0;
-
+let s= 0;
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
@@ -14,11 +14,32 @@ hantei();
 hantei();
 
 // ボタンを押した後の処理をする関数 hantei() の定義
-function hantei() {
+  function hantei() {
+    let b = document.querySelector('#print');
+    b.addEventListener('click', greeting);
+
+// 2. イベントハンドラの定義
+function greeting() {
+	let i = document.querySelector('input[name="seisuu"]');
+	let seisuu = i.value;
+	let yoso = seisuu;       
+    kaisu++;
+    let presult = document.querySelector('p#result');
+    span = document.createElement('span');
+    presult.insertAdjacentElement('beforeend',span);
+
+
+    span.textContent = (kaisu+"回目の予想："+yoso+"  ");
+
+    let presult2 = document.querySelector('p#result2');
+    li = document.createElement('li');
+    presult2.insertAdjacentElement('afterend',li);
+
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  let yoso = 4;
+  
   
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
 }
+  }
