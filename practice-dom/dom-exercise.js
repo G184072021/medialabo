0e88ad7da36def090f6d8671d70766bded796ba9
G184072021 <218407@st.takushoku-u.ac.jp>
2024-05-23 15:52:12 +0900

    a

--- practice-dom/dom-exercise.js
+++ practice-dom/dom-exercise.js
@@ -14,7 +14,7 @@ let data = [
 //// 注意: 以上は編集しないこと!
 
 // 練習4-2 メッセージ追加プログラム
-let p = document.createElement('p'); 
+/*let p = document.createElement('p'); 
 p.textContent = '写真表と都市の緯度経度のページです'; 
 let h2 = document.querySelector('h2#ex42');
 h2.insertAdjacentElement('afterend', p); 
@@ -43,5 +43,4 @@ for (let student of students) {
 
 
 
-// 練習4-5 箇条書き追加プログラム
-
+// 練習4-5 箇条書き追加プログラム*/
