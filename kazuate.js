afdf713049ec8a53bb864412f69bee4e90c46c23
G184072021 <218407@st.takushoku-u.ac.jp>
2024-05-25 14:05:14 +0900

    a

--- kazuate.js
+++ kazuate.js
@@ -4,13 +4,13 @@ console.log('答え: ' + kotae);
 
 // 入力回数（予想回数）
 let kaisu = 0;
-let s= 0;
+//let s= 0;
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
 
 // ボタンを押した後の処理をする関数 hantei() の定義
-  function hantei() {
+
     let b = document.querySelector('#kaito');
     b.addEventListener('click', hantei);
 
@@ -19,19 +19,15 @@ function hantei() {
 	let i = document.querySelector('input[name="seisuu"]');
 	let yoso = i.value;
 	//let yoso = seisuu;       
-    kaisu++;
-    let span_kaisu = document.querySelector('span#kaisu');
-    span = document.createElement('span');
-    presult.insertAdjacentElement('beforeend',span);
+    
+    let span_kaisu = document.querySelector('span#kaisuu');
+    span_kaisuu.textContent=kaisuu+1;
 
+    let span_answer = document.querySelector('span#answer');
+    span_answer.textContent=yoso;
+    let p_result=document.querySelector('p#result');
 
-    span.textContent = (kaisu+"回目の予想："+yoso+"  ");
-
-    let presult2 = document.querySelector('p#result2');
-    li = document.createElement('li');
-    presult2.insertAdjacentElement('afterend',li);
-
-  // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
+  
   
   
 //        ページに表示する方法はまだ習っていないので
@@ -39,6 +35,8 @@ function hantei() {
 
   
   // 課題3-1: 正解判定する
+console.log(kaisuu+1+"回の予想:"+yoso);
+
   if(kotae == yoso){
     li.textContent = ("正解です。おめでとう！");
     console.log = ("正解です。おめでとう！");
@@ -58,5 +56,5 @@ else if(kotae < yoso){
 }
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
-}
+
   
