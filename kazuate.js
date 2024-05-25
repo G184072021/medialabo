ec619ddfff2a5ee77d55d854f241b08e4d666c64
G184072021 <218407@st.takushoku-u.ac.jp>
2024-05-25 14:08:31 +0900

    a

--- kazuate.js
+++ kazuate.js
@@ -3,7 +3,7 @@ let kotae = Math.floor(Math.random()*10) + 1;
 console.log('答え: ' + kotae);
 
 // 入力回数（予想回数）
-let kaisu = 0;
+let kaisuu = 0;
 //let s= 0;
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
