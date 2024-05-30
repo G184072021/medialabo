c575ccbb600f9afb85108759d63ca666d831d108
G184072021 <218407@st.takushoku-u.ac.jp>
2024-05-30 09:46:34 +0900

    a

--- kazuate.js
+++ kazuate.js
@@ -3,20 +3,17 @@ let kotae = Math.floor(Math.random()*10) + 1;
 console.log('答え: ' + kotae);
 
 // 入力回数（予想回数）
-let kaisuu = 0;
+let kaisu = 0;
 
     let b = document.querySelector('#kaito');
     b.addEventListener('click', hantei);
-let w=document.querySelector('p#result');
-  w.remove();
 // 2. イベントハンドラの定義
 function hantei() {
-	let i = document.querySelector('input[name="seisuu"]');
-	let yoso = i.value;
-	//let yoso = seisuu;       
+	let i = document.querySelector('input[name="seisu"]');
+	let yoso = i.value;     
     
-    let span_kaisuu = document.querySelector('span#kaisuu');
-    span_kaisuu.textContent=kaisuu+1;
+    let span_kaisu = document.querySelector('span#kaisu');
+    span_kaisu.textContent=kaisu+1;
 
     let span_answer = document.querySelector('span#answer');
     span_answer.textContent=yoso;
@@ -31,16 +28,16 @@ function hantei() {
 
   
   // 課題3-1: 正解判定する
-console.log(kaisuu+1+"回の予想:"+yoso);
+console.log(kaisu+1+"回の予想:"+yoso);
 
-  if(kaisuu>=3){
+  if(kaisu>=3){
    p_result.textContent="答えは"+kotae+"でした。すでにゲームは終わっています。";
 }
 else if(yoso==kotae){
   p_result.textContent = "正解です。おめでとう！";
     
 }
-else if(kaisuu==2){
+else if(kaisu==2){
   p_result.textContent ="間違い。残念でした答えは"+kotae+"でした。";
     
 }
@@ -51,7 +48,7 @@ else if(yoso<kotae){
 else{
   p_result.textContent="間違い。答えはもっと小さいです。";
 }
-kaisuu=kaisuu+1;
+kaisu=kaisuu+1;
 }
 
 
